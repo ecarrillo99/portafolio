5d4701c78fd5cc37198ebc1a791e3190d56f42d9
Edisson <carrilloedisson@gmail.com>
2023-05-10 11:13:23 -0500

    nueva build 7

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,11 +11,11 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           {/*<MailchimpForm />*/}
-          <Col size={12} sm={6}>
+          <Col size={12} sm={6} className="text-center text-sm-start">
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
+            <div className="social-icon ">
               <a href="https://www.linkedin.com/in/carrillo-edisson/"><img src={navIcon1} alt="Icon" /></a>
               {/*<a href="#"><img src={navIcon2} alt="Icon" /></a>*/}
               <a href="https://www.instagram.com/edisson_carrillo/"><img src={navIcon3} alt="Icon" /></a>
